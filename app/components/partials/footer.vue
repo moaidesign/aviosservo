e5da0fec95d901465b5188994b3f16ebb9f97a6a
Oxisnet <41337924+moaidesign@users.noreply.github.com>
2020-07-04 23:42:25 +0200

    site status in footer

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -13,6 +13,7 @@
           xmlns="http://www.w3.org/2000/svg"
           xmlns:xlink="http://www.w3.org/1999/xlink"
         >
+        [![Netlify Status](https://api.netlify.com/api/v1/badges/49a0ba5a-5b84-4d50-84a0-f72a7ea089ed/deploy-status)](https://app.netlify.com/sites/aviosservo/deploys)
           <g>
             <g>
               <g>
